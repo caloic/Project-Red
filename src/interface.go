7fa8d10fb1416fa3f5a3bfeaa04530ef53af04f0
caloic <loic.cano@ynov.com>
2023-09-22 11:37:43 +0200

    [UPDATE] Création du fichier interface.go avec le logo du jeu ainsi que le background provisoir

--- src/interface.go
+++ src/interface.go
@@ -13,12 +13,53 @@ func main() {
 	background := rl.LoadTexture("background.jpg")
 	logo := rl.LoadTexture("logo.png")
 
+	// Calculer les coordonnées x et y pour centrer le logo
+	screenWidth := int32(rl.GetScreenWidth())
+	screenHeight := int32(rl.GetScreenHeight())
+	logoX := (screenWidth - int32(logo.Width)) / 2
+	logoY := (screenHeight - int32(logo.Height)) / 2
+
+	// Définir les propriétés du bouton "Play"
+	playButtonWidth := int32(200)
+	playButtonHeight := int32(50)
+	playButtonX := (screenWidth - playButtonWidth) / 2
+	playButtonY := logoY + int32(logo.Height) + 20
+	playButtonColor := rl.NewColor(150, 150, 150, 255) // Couleur du bouton "Play"
+
+	// Définir les propriétés du bouton "Quitter"
+	quitButtonWidth := int32(200)
+	quitButtonHeight := int32(50)
+	quitButtonX := (screenWidth - quitButtonWidth) / 2
+	quitButtonY := playButtonY + playButtonHeight + 20
+	quitButtonColor := rl.NewColor(150, 150, 150, 255) // Couleur du bouton "Quitter"
+
 	for !rl.WindowShouldClose() {
+		// Détecter le clic de la souris
+		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			mousePos := rl.GetMousePosition()
+
+			// Vérifier si le clic est sur le bouton "Quitter"
+			if mousePos.X >= float32(quitButtonX) && mousePos.X <= float32(quitButtonX+quitButtonWidth) &&
+				mousePos.Y >= float32(quitButtonY) && mousePos.Y <= float32(quitButtonY+quitButtonHeight) {
+				// Fermer la fenêtre si le bouton "Quitter" est cliqué
+				rl.CloseWindow()
+			}
+		}
+
 		rl.BeginDrawing()
 
-		// Impoter le backgroud / logo
+		// Importer le background / logo
 		rl.DrawTexture(background, 0, 0, rl.RayWhite)
-		rl.DrawTexture(logo, 100, 100, rl.RayWhite)
+		rl.DrawTexture(logo, logoX, logoY, rl.RayWhite)
+
+		// Dessiner le bouton "Play"
+		rl.DrawRectangle(playButtonX, playButtonY, playButtonWidth, playButtonHeight, playButtonColor)
+		rl.DrawText("Jouer", int32(playButtonX)+10, playButtonY+10, 30, rl.Black)
+
+		// Dessiner le bouton "Quitter"
+		rl.DrawRectangle(quitButtonX, quitButtonY, quitButtonWidth, quitButtonHeight, quitButtonColor)
+		rl.DrawText("Quitter", int32(quitButtonX)+10, quitButtonY+10, 30, rl.Black)
+
 		rl.EndDrawing()
 	}
 

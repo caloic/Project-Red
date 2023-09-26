f789faddbae72aa65f4eada43a98601b5d308457
Meca <theteampower@free.fr>
2023-09-26 11:39:57 +0200

    [UPDATE] mise a jour de playerManger.go

--- src/entity/playerManger.go
+++ src/entity/playerManger.go
@@ -5,9 +5,22 @@ import "ftl/src/enum"
 var playerList []Player
 
 func CreatePlayer() {
-	playerList = append(Player{Lifes: 20, Armors: enum.Nothing, Name: "Player"})
+	listLen := len(playerList)
+	switch listLen {
+	case 0:
+		playerList = append(playerList, Player{Lifes: 20, Armors: enum.Nothing, Name: "Player"})
+	case 1:
+		playerList = append(playerList, Player{Lifes: 20, Armors: enum.Nothing, Name: "Soldier1"})
+	case 2:
+		playerList = append(playerList, Player{Lifes: 20, Armors: enum.Nothing, Name: "Soldier2"})
+		case
+	}
 }
 
 func LooseHealth(p Player, pv int) {
 	p.Lifes = p.Lifes - pv
+}
+
+func GetPlayerList() []Player {
+	return playerList
 }
